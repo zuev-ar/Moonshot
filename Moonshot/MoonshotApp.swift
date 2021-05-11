9ac41e818c5cc8890101afced7b127e5ed883df6
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-05-11 15:42:49 +0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Arkasha Zuev on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
